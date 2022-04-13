2bdadae0609ab58bdf2e19c18cf2cfcc8a5edeb7
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-13 15:50:11 -0500

    get users implemented

--- api/server.js
+++ api/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 
+const usersRouter = require('./users/users-router.js');
+
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json());
 // global middlewares and the user's router need to be connected here
+
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || 'Error retrieving the users',
+    stack: err.error.stack,
+  });
 });
 
 module.exports = server;
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,58 @@
 const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
+const Users = require('./users-model.js');
+const Posts = require('../posts/posts-model.js');
 // The middleware functions also need to be required
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => next({ error }))
 });
 
 router.get('/:id', (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.send('unimplemented')
 });
 
 router.post('/', (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  res.send('unimplemented')
 });
 
 router.put('/:id', (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  res.send('unimplemented')
 });
 
 router.delete('/:id', (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  res.send('unimplemented')
 });
 
 router.get('/:id/posts', (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  res.send('unimplemented')
 });
 
 router.post('/:id/posts', (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  res.send('unimplemented')
 });
 
 // do not forget to export the router
+module.exports = router;
